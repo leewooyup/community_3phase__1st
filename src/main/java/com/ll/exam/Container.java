41fa55eada65aacd1b3313d633af665f638decfd
leewooyup <zjadlspun1114@naver.com>
2022-08-02 10:38:25 +0900

    Container안에 articleController 생성

---
+++ src/main/java/com/ll/exam/Container.java
@@ -0,0 +1,9 @@
+package com.ll.exam;
+
+import com.ll.exam.article.controller.ArticleController;
+
+public class Container {
+    public static ArticleController getArticleController() {
+        return new ArticleController();
+    }
+}
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,14 +1,22 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+
     @Test
     public void junit_assertThat() {
         int rs = 10 + 20;
 
         assertThat(rs).isEqualTo(30);
+    }
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
     }
 }
