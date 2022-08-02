bfe10bc60af863ce07d5799d38a68b73286ce37d
leewooyup <zjadlspun1114@naver.com>
2022-08-02 14:22:40 +0900

    Ut.cls.newObj로 객체생성

--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -1,8 +1,29 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
+import com.sun.tools.javac.code.TypeAnnotationPosition;
+
 import java.beans.Introspector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Ut {
+    public static class cls {
+
+        public static Object newObj(Class cls, Object defalutValue) {
+            try {
+                cls.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public static class str {
         public static String decapitalize(String str) {
             if(str == null || str.length() == 0) {
