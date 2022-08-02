64ffd0f4c9686af6ddb8136f8da49af8cd7632bb
leewooyup <zjadlspun1114@naver.com>
2022-08-02 10:40:40 +0900

    articleController 싱글톤 작동 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -19,4 +19,12 @@ public class AppTest {
 
         assertThat(articleController).isNotNull();
     }
+
+    @Test
+    public void ioc__articleController__싱글톤() {
+        ArticleController articleController1 = Container.getArticleController();
+        ArticleController articleController2 = Container.getArticleController();
+
+        assertThat(articleController2).isEqualTo(articleController1);
+    }
 }
